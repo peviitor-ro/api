92a6a8bf65508efb8175fb7c97ea45687c847c01
sebastianboga <sebastian.boga@endava.com>
2021-08-01 20:18:12 +0300

    adding initial commit

---
+++ index.php
@@ -0,0 +1,1 @@
+home of web API
---
+++ update/index.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+
+$method = 'POST';
+//$server = 'http://23.97.216.44/solr/';
+$server = 'http://zimbor.go.ro/solr/';
+$core  = 'shaqodoon';
+$command ='/update';
+$qs = '?_=1617366504771&commitWithin=1000&overwrite=true&wt=json';
+
+$url =  $server.$core.$command.$qs;
+ 
+$data = file_get_contents('php://input');
+
+
+
+
+
+$url = $server.$core.$command.$qs;
+
+
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => $data
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
+?>
