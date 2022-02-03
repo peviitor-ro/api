b467f9cb98fbfe6558fad76d53d46fc9ad359464
Sebastian Boga <sebastian.boga@endava.com>
2022-02-03 20:50:54 +0200

    create v3 companies

---
+++ v3/companies/index.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+   /**
+     * @OA\Get(
+     *     path="/v1/companies/", tags={"UI"},
+   *  @OA\Parameter(
+     *                in="query", 
+     *                 name="count",
+     *                 @OA\Schema(
+     *                   type="string"), 
+     *                 example="true"
+     * ),
+     *     @OA\Response(response="200", description="Success")
+     * )
+     */
+
+$url = 'https://api.peviitor.ro/v0/search/?facet.field=company&facet=on&q=*%3A*&rows=0';
+$string = file_get_contents($url);
+$json = json_decode($string, true);
+
+$companies = $json['facet_counts']['facet_fields']['company'];
+
+
+
+$results = array();
+ 
+for($i=0;$i<count($companies)/2;$i++) {
+    $k=2*$i;
+    $results[$i] = $companies[$k];
+  }
+
+
+
+
+
+echo json_encode($results);
+
+
+?>
