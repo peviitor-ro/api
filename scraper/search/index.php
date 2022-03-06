766a6e934ec6b2219305da3019127fe4c6f33f8f
Sebastian Boga <sebastian.boga@endava.com>
2022-03-06 14:01:16 +0200

    adding scraper

---
+++ scraper/search/index.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+$qs = $_SERVER['QUERY_STRING'];
+
+function get_server(){
+    //get the IP of the server
+    //we need a config file to know where is the SOLR
+    require('../../_config/index.php');
+    return $server_test;
+}
+
+ $url =  get_server().'shaqodoon/select?'.$qs;
+ $json = file_get_contents($url);
+ echo $json;
+?>
