5d4249c9a1ce77fcc8524e9313f7c69420a3b5f7
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2022-10-29 19:25:05 +0300

    Update index.php

--- v1/companies/index.php
+++ v1/companies/index.php
@@ -19,7 +19,7 @@ $url = 'https://api.peviitor.ro/v0/search/?facet.field=company_str&facet.limit=1
 $string = file_get_contents($url);
 $json = json_decode($string, true);
 
-$companies = $json['facet_counts']['facet_fields']['company'];
+$companies = $json['response']['numFound'];
 
 $results =  new stdClass();
 $results->total = count($companies)/2;
