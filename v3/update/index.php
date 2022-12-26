ecf60524515195afacbe90932ca18c4108a2a91e
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2022-12-26 15:40:33 +0200

    update v3

--- v3/update/index.php
+++ v3/update/index.php
@@ -84,6 +84,7 @@ header("Access-Control-Allow-Origin: *");
     foreach ($json as $item) {
         $item->company=$xcompany;
         $item->job_title=html_entity_decode($item->job_title);
+        $item->country    = str_replace("Romania","România",$item->country);
     }
     
 
