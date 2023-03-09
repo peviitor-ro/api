715572fe2e1f98dbc60887413721bb394664c274
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-03-09 21:34:18 +0200

    fixing the v1 like v4

--- v1/jobs/index.php
+++ v1/jobs/index.php
@@ -18,6 +18,7 @@ header("Access-Control-Allow-Origin: *");
 
 $qs = "q=*%3A*&rows=100&omitHeader=true";
 //$qs = urldecode($qs);
+if (isset($_GET["start"])) {$start=$_GET["start"];$qs.="&start=".$start;}
 $url =  'http://solr.peviitor.ro/solr/shaqodoon/select?'.$qs;
 
  
