f1801c54e1884787a2c9093a2e16d433057c07f2
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-04-26 20:31:45 +0300

    Create index.php

---
+++ v1/logo/index.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$qs = "indent=true&q.op=OR&q=logo%3A*&rows=10000&useParams=";
+
+function get_server(){
+    //get the IP of the server
+    //we need a config file to know where is the SOLR
+    require('../../_config/index.php');
+    return $server;
+}
+$core ="auth";
+$url =  get_server().$core.'/select?'.$qs;
+
+$json = file_get_contents($url);
+echo $json;
+?>
