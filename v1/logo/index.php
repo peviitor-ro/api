e457997fe4eb93d4f8ee7a685f553f11a517971e
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-04-26 21:23:38 +0300

    Update index.php

--- v1/logo/index.php
+++ v1/logo/index.php
@@ -30,8 +30,37 @@ foreach($companies as $company)
     $xurl  =  $company["logo"];
     $url  = $xurl[0];
     $test[$item] = $url;
-     var_dump($test);
+    
+}
+// var_dump($test);
+
+$url = 'https://api.peviitor.ro/v0/search/?facet.field=company_str&facet.limit=10000&facet=true&fl=company&facet.sort=index&indent=true&q.op=OR&q=*%3A*&rows=0&start=0';
+$string = file_get_contents($url);
+$json = json_decode($string, true);
+
+$companies = $json['facet_counts']['facet_fields']['company_str'];
+
+$results =  new stdClass();
+$results->total = count($companies)/2;
+$results->companies = array();
+
+for($i=0;$i<count($companies)/2;$i++) {
+    $k=2*$i;
+    $l=2*$i+1;
+    $obj = new stdClass();
+    $obj->name = $companies[$k];   
+    if (isset($_GET['count']))
+     if ($_GET['count']=='true')
+       {
+    $obj->jobs = $companies[$l];
+       }
+    $obj->logo = $test[$obj->name];
+    $results->companies[$i] = new stdClass();
+    $results->companies[$i] = $obj;
+   
+
+    
 }
 
-//echo json_encode($results);
+echo json_encode($results);
 ?>
