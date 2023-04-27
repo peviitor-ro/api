d8a798ef77d591f43e4a2c5dc938db0ae2d6c41e
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-04-27 12:48:33 +0300

    Update index.php

--- v1/logo/add/index.php
+++ v1/logo/add/index.php
@@ -1,1 +1,33 @@
+<?php
+header("Access-Control-Allow-Origin: *");
 
+$method = 'POST';
+$server = 'http://zimbor.go.ro/solr/';
+$core  = 'auth';
+$command ='/update';
+$qs = '?_=1617366504771&commitWithin=1000&overwrite=true&wt=json';
+
+$url =  $server.$core.$command.$qs;
+ 
+$data = file_get_contents('php://input');
+$json = json_decode($data);
+print_r($data);
+   foreach ($json as $item) {
+    $item->id=html_entity_decode($item->company);
+    $item->logo = $item->logo;
+    
+}
+
+$data = json_encode($json);
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => $data
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+?>
