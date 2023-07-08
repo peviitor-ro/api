f2edb0f4f887c74f0c6976fee5f2c7901d0bc450
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-07-08 12:04:42 +0300

    get the user from api key and push to DISCORD

--- v4/update/index.php
+++ v4/update/index.php
@@ -35,6 +35,33 @@ header("Access-Control-Allow-Origin: *");
   return $x;
  }
 
+
+ function get_user_from_api_key($key) {
+    $method = 'GET';
+    $server = get_server();
+    $core  = 'auth';
+    $command ='/select';
+    $qs = '?q.op=OR&q=apikey%3A"'.$key.'"&rows=0';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+
+     $y = $json->response->numFound; 
+    if ($y==1) {$x = $json->response->docs[0]->id;}
+    if ($y==0) {$x = false;}
+  return $x;
+ }
+
  function get_company($token) {
     $x=$company = $_POST['company'];
    return $x;
@@ -97,7 +124,7 @@ function discord_webhook($msg) {
             'content' => $data
         )
     );
-    $msg = $company.' key-> '.$key.' user-> ...(still working on this)';
+    $msg = $company.' key-> '.$key.' user-> '.get_user_from_api_key($key);
     discord_webhook($msg);
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
