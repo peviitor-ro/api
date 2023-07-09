4b7455fcd39ccf03fc632521464d092684d9443c
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-07-09 18:39:50 +0300

    updating v1 clean

--- v1/clean/index.php
+++ v1/clean/index.php
@@ -24,10 +24,48 @@ header("Access-Control-Allow-Origin: *");
 
 
 
+     function get_server(){
+        //get the IP of the server
+        //we need a config file to know where is the SOLR
+        require('../../_config/index.php');
+        return $server;
+    }
+
+
+    function company_exist($company) {
+        $url = 'https://api.peviitor.ro/v0/search/?https://solr.peviitor.ro/solr/jobs/select?indent=true&q.op=OR&q=company%3A%22'.$company.'%22&rows=0&useParams=';
+        $string = file_get_contents($url);
+        $json = json_decode($string, true);
+        
+        if ($json['response']['numFound']==0) {return "new";}
+        if ($json['response']['numFound']!=0) {return "existing";}
+    
+         }
+
+    function discord_webhook($msg) {
+       
+            $msg .= ' CLEAN in PRODUCTION at '. date("l d-m-Y H:i:s"); 
+            $method = 'POST';
+            $url = "https://discord.com/api/webhooks/1127592366614786118/ZOcdq94sqxO4P8iOIkQdRLG9s_vwgRfg1DFxhybwpHkqyet0QTe33rQ7bSDS5AG5HP8n";
+            $data = '{"content": "'.$msg.'"}';
+        
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/json\r\n",
+                    'method'  => 'POST',
+                    'content' => $data
+                )
+            );
+            $context  = stream_context_create($options);
+            $result = file_get_contents($url, false, $context);
+            if ($result === FALSE) { /* Handle error */ }
+           
+    }         
+
 $method = 'POST';
 
 
-$server = 'http://zimbor.go.ro/solr/';
+$server = get_server();;
 $core  = 'shaqodoon';
 $command ='/update';
 $qs = '?_=1617366504771&commitWithin=1000&overwrite=true&wt=json';
@@ -54,6 +92,10 @@ $options = array(
         'content' => $data
     )
 );
+$msg='';
+    if (company_exist($company)=="new") {$msg.="!!! COMPLETELY NEW  !!!";}
+    $msg .= $company;
+    discord_webhook($msg);
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 if ($result === FALSE) { /* Handle error */ }
