1c901a379f702464abca2ca15917dd41ff18b49d
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-07-10 00:51:59 +0300

    creating a v0 search test

---
+++ v0/search/test/index.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$qs = $_SERVER['QUERY_STRING'];
+function get_server(){
+    //get the IP of the server
+    //we need a config file to know where is the SOLR
+    require('../../../_config/index.php');
+    return $server;
+}
+$core ="shaqodoon";
+$url =  get_server().$core.'/select?'.$qs;
+$json = file_get_contents($url);
+echo $json;
+?>
