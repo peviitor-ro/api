92d13db3de945cf789ac237b01d0561df0bcdfa0
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-16 16:20:52 +0300

    Update index.php

--- v0/search/index.php
+++ v0/search/index.php
@@ -1,9 +1,6 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
-
 $qs = $_SERVER['QUERY_STRING'];
-
 function get_server(){
     //get the IP of the server
     //we need a config file to know where is the SOLR
@@ -11,24 +8,7 @@ function get_server(){
     return $server;
 }
 $core ="jobs";
-$url =  get_server().$core.'/select?'.$qs;
-
- 
-
- 
-
+$url =  get_server()[0].$core.'/select?'.$qs;
 $json = file_get_contents($url);
-
-
-
-
-
 echo $json;
-
-
-
-
-
-
-
 ?>
