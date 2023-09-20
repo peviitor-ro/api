0478b83fba93daf193d2fa32055276349e2902f3
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 01:50:23 +0300

    Update index.php

--- v0/server/index.php
+++ v0/server/index.php
@@ -42,10 +42,8 @@ function isSolrServerUp($solrUrl, $coreName) {
 }
 
 $message = array();
-//$server = get_server();
-   $server = array();
-   $server[0] = 'http://peviitor.go.ro/solr/';
-   $server[1] = 'http://zimbor.go.ro/solr/';
+$server = get_server();
+   
    
 foreach ($server as $solrUrl) {
  $msg = new stdClass();
