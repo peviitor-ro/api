7bd1f5c3d6248508b7f580360649677c8ddad719
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 00:59:17 +0300

    Update index.php

--- v1/getuser/index.php
+++ v1/getuser/index.php
@@ -1,6 +1,33 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+function get_master_server(){
+    $method = 'GET';
+    $server = "https://api.peviitor.ro/";
+    $core  = 'v0';
+    $command ='/server/';
+    $qs = '';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+    foreach($json as $item)
+        {
+            if ($item->status=="up"){
+                return $item->server;
+                break;
+            }
+        }
+}
 
 $qs = $_SERVER['QUERY_STRING'];
 $qs = urldecode($qs);
@@ -10,8 +37,8 @@ if (isset($_GET['user']))
 $user = $_GET['user'];
 $user = urlencode($user);
 
-$server = 'solr.peviitor.ro';
-$url =  'https://'.$server.'/solr/auth/select?'.'omitHeader=true&q.op=OR&q=id%3A'.$user;
+$server = get_master_server();
+$url =  '/auth/select?'.'omitHeader=true&q.op=OR&q=id%3A'.$user;
 $json = file_get_contents($url);
 $json = json_decode($json);
 unset($json->response->docs[0]->_version_);
