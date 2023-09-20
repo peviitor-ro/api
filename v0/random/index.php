b808a95db4d3cd214b673b1a5d50083bfa1ac1d0
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 00:44:28 +0300

    Update index.php

--- v0/random/index.php
+++ v0/random/index.php
@@ -11,25 +11,46 @@ header("Access-Control-Allow-Headers: *");
      * )
      */
 
-    function get_server(){
-        //get the IP of the server
-        //we need a config file to know where is the SOLR
-        require('../../_config/index.php');
-        return $server;
-    }
+   function get_master_server(){
+    $method = 'GET';
+    $server = "https://api.peviitor.ro/";
+    $core  = 'v0';
+    $command ='/server/';
+    $qs = '';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+    foreach($json as $item)
+        {
+            if ($item->status=="up"){
+                return $item->server;
+                break;
+            }
+        }
+}
 	
 
     $url = 'https://api.peviitor.ro/v0/search/?q=*%3A*&rows=0';
     $string = file_get_contents($url);
     $json = json_decode($string, true);
     
-$server = get_server();	
+$server = get_master_server();	
 
 $max=$json['response']['numFound'];;
 $start = rand(0,$max);
 $qs = 'q=*%3A*&rows=1&start='.$start.'&omitHeader=true';
 $core = "jobs";
-$url =  $server[0].$core.'/select?'.$qs;
+$url =  $server.$core.'/select?'.$qs;
  
  
 $json = file_get_contents($url);
