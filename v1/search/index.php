a455d10f9d15606f15e7664433c425044ef95b62
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 01:13:00 +0300

    Update index.php

--- v1/search/index.php
+++ v1/search/index.php
@@ -47,7 +47,33 @@ function get_server(){
      * )
      */
 
-
+   function get_master_server(){
+    $method = 'GET';
+    $server = "https://api.peviitor.ro/";
+    $core  = 'v0';
+    $command ='/server/';
+    $qs = '';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+    foreach($json as $item)
+        {
+            if ($item->status=="up"){
+                return $item->server;
+                break;
+            }
+        }
+}
 
 if (isset($_GET['q'])) {$q  = "q=".urlencode($_GET['q']);} else {$q='q=*:*';}
 if (isset($_GET['company'])) {$q .= "&fq=company:".urlencode($_GET['company']);}
@@ -60,7 +86,7 @@ if (isset($_GET['page'])) {
     $q .= "&start=".$start;
 }
 
-$url =  get_server()[0].'shaqodoon/select?'.$q;
+$url =  get_master_server().'shaqodoon/select?'.$q;
 $json = file_get_contents($url);
 echo $json;
 ?>
