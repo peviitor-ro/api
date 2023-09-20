7a34770d0da8228e3bd4e089cfe83edf3b7c4e86
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 01:00:21 +0300

    Update index.php

--- v1/jobs/index.php
+++ v1/jobs/index.php
@@ -15,11 +15,38 @@ header("Access-Control-Allow-Origin: *");
      * )
      */
 
+function get_master_server(){
+    $method = 'GET';
+    $server = "https://api.peviitor.ro/";
+    $core  = 'v0';
+    $command ='/server/';
+    $qs = '';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+    foreach($json as $item)
+        {
+            if ($item->status=="up"){
+                return $item->server;
+                break;
+            }
+        }
+}
 
 $qs = "q=*%3A*&rows=100&omitHeader=true";
 //$qs = urldecode($qs);
 if (isset($_GET["start"])) {$start=$_GET["start"];$qs.="&start=".$start;}
-$url =  'http://solr.peviitor.ro/solr/shaqodoon/select?'.$qs;
+$url =  get_master_server().'shaqodoon/select?'.$qs;
 $json = file_get_contents($url);
 echo $json;
 ?>
