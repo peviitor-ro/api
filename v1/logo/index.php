2c053a5a193bb6fe172997817456fc911b4c78f7
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 01:07:14 +0300

    Update index.php

--- v1/logo/index.php
+++ v1/logo/index.php
@@ -8,8 +8,37 @@ function get_server(){
     require('../../_config/index.php');
     return $server;
 }
+
+   function get_master_server(){
+    $method = 'GET';
+    $server = "https://api.peviitor.ro/";
+    $core  = 'v0';
+    $command ='/server/';
+    $qs = '';
+    $url =  $server.$core.$command.$qs;
+   
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    $json = json_decode($result);
+    foreach($json as $item)
+        {
+            if ($item->status=="up"){
+                return $item->server;
+                break;
+            }
+        }
+}
+
 $core ="auth";
-$url =  get_server()[0].$core.'/select?'.$qs;
+$url =  get_master_server().$core.'/select?'.$qs;
 
 $string = file_get_contents($url);
 $json = json_decode($string, true);
