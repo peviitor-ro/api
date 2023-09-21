6c49342ac0d830110f19733df61f4fc3cc0f0a1e
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-22 00:52:49 +0300

    Update index.php

--- v0/server/test/setsolr/index.php
+++ v0/server/test/setsolr/index.php
@@ -1,13 +1,78 @@
 <?php
+//$data = "https://solr.peviitor.ro/solr/";
+
+ function get_server(){
+        //get the IP of the server
+        //we need a config file to know where is the SOLR
+        require('../../../../_config/index.php');
+        return $server;
+    }
+
+
+$coreName = 'jobs'; // Name of your SOLR core
+
+// Function to check if SOLR server is up and running
+function isSolrServerUp($solrUrl, $coreName) {
+    $pingUrl = $solrUrl .  $coreName . '/admin/ping?wt=json';
+
+    // Initialize cURL session
+    $ch = curl_init($pingUrl);
+
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    // Execute cURL request
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    // Close cURL session
+    curl_close($ch);
+
+    // Check the HTTP response code
+    if ($httpCode === 200) {
+        $data = json_decode($response, true);
+
+        // Check if SOLR is up and running based on the response
+        if (isset($data['status']) && $data['status'] === 'OK') {
+            return true;
+        }
+    }
+
+    return false;
+}
+$message = array();
+   $server = array();
+   $server[0] = 'https://solrcluj.peviitor.ro/solr/';
+   $server[1] = 'https://solr.peviitor.ro/solr/';
+   
+   
+foreach ($server as $solrUrl) {
+ $msg = new stdClass();
+
+if (isSolrServerUp($solrUrl, $coreName))
+{
+  
+  $msg->server = $solrUrl;
+  $msg->status = "up";
+    $data = $solrUrl;
+    break;
+} else {
+    $msg->server = $solrUrl;
+    $msg->status = "down";
+}
+ $message[]= $msg; 
+}
+
 // Data to be written to the file
-$data = "https://solr.peviitor.ro/solr/";
+
 
 // File path
 $filePath = "../server.txt";
 
 // Write data to the file
 if (file_put_contents($filePath, $data) !== false) {
-    echo "Data has been written to the file successfully.";
+    echo $data;
 } else {
     echo "Unable to write data to the file.";
 }
