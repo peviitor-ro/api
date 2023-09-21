a2aef37d2a5c8404172100314bbd3da213b0360d
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-22 00:42:28 +0300

    Update index.php

--- v0/server/test/getsolr/index.php
+++ v0/server/test/getsolr/index.php
@@ -1,5 +1,14 @@
 <?php
+// File path
+$filePath = "../server.txt";
 
-$server_bun = getenv('SOLR_SERVER');
-echo $server_bun;
+// Read the contents of the file into a string
+$fileContents = file_get_contents($filePath);
+
+if ($fileContents !== false) {
+    echo "File contents:\n";
+    echo $fileContents;
+} else {
+    echo "Unable to read the file.";
+}
 ?>
