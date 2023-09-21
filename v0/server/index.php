e5ca39bf0998a53d7b902fc4a47bb842e561d298
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-21 10:24:53 +0300

    Update index.php

--- v0/server/index.php
+++ v0/server/index.php
@@ -12,18 +12,17 @@ $coreName = 'jobs'; // Name of your SOLR core
 
 // Function to check if SOLR server is up and running
 function isSolrServerUp($solrUrl, $coreName) {
-    $pingUrl = $solrUrl  . $coreName . '/admin/ping?wt=json';
+    $pingUrl = $solrUrl . '/' . $coreName . '/admin/ping?wt=json';
 
-    // Initialize cURL session with an HTTP HEAD request
+    // Initialize cURL session
     $ch = curl_init($pingUrl);
 
-    // Set cURL options for an HTTP HEAD request
-    curl_setopt($ch, CURLOPT_NOBODY, true); // Use HEAD request
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false); // Do not return the response body
-    curl_setopt($ch, CURLOPT_TIMEOUT, 1); // Wait for a maximum of 1 second for a response
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
     // Execute cURL request
-    curl_exec($ch);
+    $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
     // Close cURL session
@@ -31,12 +30,16 @@ function isSolrServerUp($solrUrl, $coreName) {
 
     // Check the HTTP response code
     if ($httpCode === 200) {
-        return true; // Server is up
+        $data = json_decode($response, true);
+
+        // Check if SOLR is up and running based on the response
+        if (isset($data['status']) && $data['status'] === 'OK') {
+            return true;
+        }
     }
 
-    return false; // Server is down or not responding
+    return false;
 }
-
 $message = array();
 $server = get_server();
    
