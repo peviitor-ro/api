4acbb800a16e50f9ee882ddf264a36158045de18
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-22 00:34:42 +0300

    Update index.php

--- v0/server/test/setsolr/index.php
+++ v0/server/test/setsolr/index.php
@@ -1,3 +1,14 @@
 <?php
-putenv("SOLR_SERVER=https://solr.peviitor.ro/solr/");
+// Data to be written to the file
+$data = "Hello, World!";
+
+// File path
+$filePath = "example.txt";
+
+// Write data to the file
+if (file_put_contents($filePath, $data) !== false) {
+    echo "Data has been written to the file successfully.";
+} else {
+    echo "Unable to write data to the file.";
+}
 ?>
