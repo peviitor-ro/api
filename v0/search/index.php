77e128139f4a7702d774df9805f157c8a999099f
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-09-22 15:26:36 +0300

    Update index.php

--- v0/search/index.php
+++ v0/search/index.php
@@ -3,33 +3,21 @@ header("Access-Control-Allow-Origin: *");
 $qs = $_SERVER['QUERY_STRING'];
 
 
-function get_master_server(){
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command ='/server/';
-    $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET',
-            'content' => $data
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
-        }
-}
+
+    function get_master_server(){
+      // File path
+        $filePath = "../server/test/server.txt";
+
+        // Read the contents of the file into a string
+           $fileContents = file_get_contents($filePath);
+
+            if ($fileContents !== false) {$server = $fileContents;
+                     } else {  $server = 'https://solr.peviitor.ro/solr/';
+                             }
+                       return $server;
+                                }
+
+
 
 $core ="jobs";
 $url =  get_master_server().$core.'/select?'.$qs;
