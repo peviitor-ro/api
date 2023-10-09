ccbcc7fa964f1023363d4fc908fdaad05cae2275
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-10-09 14:49:57 +0300

    fix number 2

--- v1/logos/index.php
+++ v1/logos/index.php
@@ -52,8 +52,8 @@ for($i=0;$i<count($companies);$i++) {
    
   
     $obj = new stdClass();
-    $obj->name = $companies[$i]->id;   
-    $obj->logo =$companies[$i]->logo[0];
+    $obj->name = $companies[$i]['id'];   
+    $obj->logo =$companies[$i]->['logo'][0];
     $results->companies[$i] = new stdClass();
     $results->companies[$i] = $obj;
    
