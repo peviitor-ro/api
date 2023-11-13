8383bbd58e3f6c357bc60ed2413d2183e535dceb
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-11-13 19:59:03 +0200

    add county

--- v3/search/index.php
+++ v3/search/index.php
@@ -23,6 +23,7 @@ if (isset($_GET['q'])) {$q  = "q=".urlencode($_GET['q']);} else {$q='q=*:*';}
 if (isset($_GET['company'])) {$q .= "&fq=company:".urlencode($_GET['company']);}
 if (isset($_GET['city'])) {$q .= '&fq=city%3A"'.urlencode($_GET['city']).'"';}
 if (isset($_GET['country'])) {$q .= "&fq=country:".urlencode($_GET['country']);}
+if (isset($_GET['county'])) {$q .= "&fq=county:".urlencode($_GET['county']);}
 if (isset($_GET['remote'])) {$q .= "&fq=remote:".urlencode($_GET['remote']);}
 if (isset($_GET['page'])) {
     $start = $_GET['page'];
