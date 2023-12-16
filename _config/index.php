1ce31f2479d6372049aa940bb461953ad2869e62
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-16 21:17:02 +0200

    Update index.php

--- _config/index.php
+++ _config/index.php
@@ -1,5 +1,7 @@
 <?php   
    $server = array();
-   $server[0] = 'https://solr.peviitor.ro/solr/';
-   $server[1] = 'https://solrcluj.peviitor.ro/solr/';
+   
+   //$server[0] = 'https://solr.peviitor.ro/solr/';
+   //$server[1] = 'https://solrcluj.peviitor.ro/solr/';
+   $server[0] = 'http://zimbor.go.ro/solr/';
 ?>
