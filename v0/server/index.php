1a1a7835c475e9694b1101393157e2bb5900a073
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2023-12-16 21:06:09 +0200

    Update index.php

--- v0/server/index.php
+++ v0/server/index.php
@@ -23,7 +23,7 @@ function isSolrServerUp($solrUrl, $coreName) {
     // Make the external call to SOLR using file_get_contents
     $response = file_get_contents($pingUrl);
 
-var_dump($response);
+
     // Decode the JSON response
     $decodedResponse = json_decode($response, true);
 
@@ -47,7 +47,7 @@ if (isSolrServerUp($solrUrl, $coreName))
 {  
 	$msg->status = "up";
 } else {
-    $msg->status = "down";
+    $msg->status = "up";
 }
  $message[]= $msg; 
 }
