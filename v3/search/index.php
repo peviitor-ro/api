29dc884062e61ffc2d6e1226d3b712cbeba071ff
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-01-28 11:49:33 +0200

    Update index.php

--- v3/search/index.php
+++ v3/search/index.php
@@ -25,7 +25,7 @@ if (isset($_GET['city'])) {$q .= "&fq=city:".urlencode($_GET['city']);
                            $q = $q."%20OR%20remote%3Aremote%20OR%20city%3Aall%20OR%20county%3Aall";
                           }
 if (isset($_GET['country'])) {$q .= "&fq=country:".urlencode($_GET['country']);}
-if (isset($_GET['county'])) {$q .= "&fq=county:".urlencode($_GET['county']);}
+//if (isset($_GET['county'])) {$q .= "&fq=county:".urlencode($_GET['county']);}
 if (isset($_GET['remote'])) {$q .= "&fq=remote:".urlencode($_GET['remote']);}
 if (isset($_GET['page'])) {
     $start = $_GET['page'];
