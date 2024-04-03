8a2c87c1b8ee707a2861a716cda9c1a7623c3ad0
ale23yfm <aleifrim79@gmail.com>
2024-04-03 18:30:34 +0300

    Update index.php

--- v1/random/index.php
+++ v1/random/index.php
@@ -1,22 +1,25 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-
-    /**
-     * @OA\Get(
-     *     path="/v1/random/", tags={"search engine"},
-     *     @OA\Response(response="200", description="Success")
-     * )
-     */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: *');
 
 
-   function get_master_server(){
+/**
+ * @OA\Get(
+ *     path="/v1/random/", tags={"search engine"},
+ *     @OA\Response(response="200", description="Success")
+ * )
+ */
+
+function get_master_server()
+{
     $method = 'GET';
     $server = "https://api.peviitor.ro/";
     $core  = 'v0';
-    $command ='/server/';
+    $command = '/server/';
     $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
+    $url =  $server . $core . $command . $qs;
+
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/json\r\n",
@@ -26,22 +29,40 @@ header("Access-Control-Allow-Origin: *");
     );
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
+    if ($result === FALSE) { /* Handle error */
+    }
     $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
+    foreach ($json as $item) {
+        if ($item->status == "up") {
+            return $item->server;
+            break;
         }
+    }
 }
 
-$max=50000; // max should be extracted from SOLR not hardcoded -- TODO!!!
-$start = rand(0,$max);
-$qs = 'q=*%3A*&rows=1&start='.$start.'&omitHeader=true';
- $url =  get_master_server().'jobs/select?'.$qs;
-  
+$core = "jobs"; //production
+$qs = '?';
+$qs = $qs . 'q=' . urlencode('*:*');
+$qs = $qs . '&';
+$qs = $qs . 'rows=0';
+$url = 'http://zimbor.go.ro/solr/' . $core . '/select' . $qs;
+$string = file_get_contents($url);
+$json = json_decode($string, true);
+
+$server = get_master_server();
+
+$max = $json['response']['numFound'];
+$start = rand(0, $max);
+$qs = 'q=' . urlencode('*:*'); //query string
+$qs = $qs . '&';
+$qs = $qs . 'rows=1';
+$qs = $qs . '&';
+$qs = $qs . 'start=' . $start;
+$qs = $qs . '&';
+$qs = $qs . 'omitHeader=true';
+$url =  $server . $core . '/select?' . $qs;
+
+
 $json = file_get_contents($url);
 echo $json;
-?>
+//to do: to add unit tests
