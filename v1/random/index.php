343e9b0f83a84334ca3faaeb5fd882258cbadf3b
ale23yfm <aleifrim79@gmail.com>
2024-04-05 19:37:09 +0300

    Updated random to v0, v1, v3, v4 from backward compatibility

--- v1/random/index.php
+++ v1/random/index.php
@@ -11,55 +11,26 @@ header('Access-Control-Allow-Headers: *');
  * )
  */
 
-function get_master_server()
-{
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command = '/server/';
-    $qs = '';
-    $url =  $server . $core . $command . $qs;
-
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET'
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */
-    }
-    $json = json_decode($result);
-    foreach ($json as $item) {
-        if ($item->status == "up") {
-            return $item->server;
-            break;
-        }
-    }
-}
-
-$core = "jobs"; //production
+$server = 'zimbor.go.ro';
+$core = 'shaqodoon'; //QA
 $qs = '?';
 $qs = $qs . 'q=' . urlencode('*:*');
 $qs = $qs . '&';
 $qs = $qs . 'rows=0';
-$url = 'http://zimbor.go.ro/solr/' . $core . '/select' . $qs;
+$url = 'http://' .$server .'/solr/' . $core . '/select' . $qs;
 $string = file_get_contents($url);
 $json = json_decode($string, true);
 
-$server = get_master_server();
-
 $max = $json['response']['numFound'];
 $start = rand(0, $max);
-$qs = 'q=' . urlencode('*:*'); //query string
+$qs = '?q=' . urlencode('*:*'); //query string
 $qs = $qs . '&';
 $qs = $qs . 'rows=1';
 $qs = $qs . '&';
 $qs = $qs . 'start=' . $start;
 $qs = $qs . '&';
 $qs = $qs . 'omitHeader=true';
-$url =  $server . $core . '/select?' . $qs;
+$url = 'http://' .$server .'/solr/' . $core . '/select' . $qs;
 
 
 $json = file_get_contents($url);
