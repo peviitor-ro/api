75c8e0aac07ce0cc93161ddc6878d5e06ee63c70
ale23yfm <aleifrim79@gmail.com>
2024-04-06 20:16:33 +0300

    Update index.php

--- v4/search/index.php
+++ v4/search/index.php
@@ -1,24 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-function get_master_server(){
-// File path
-$filePath = "../../v0/server/test/server.txt";
-
-// Read the contents of the file into a string
-$fileContents = file_get_contents($filePath);
-
-if ($fileContents !== false) {
-       $server = $fileContents;
-} else {
-    $server = 'https://solr.peviitor.ro/solr/';
-}
-    return $server;
-}
-
-    
-
-
 if (isset($_GET['q'])) {$q  = "q=".urlencode($_GET['q']);} else {$q='q=*:*';}
 if (isset($_GET['company'])) {$q .= "&fq=company:".urlencode($_GET['company']);}
 if (isset($_GET['city'])) {$q .= '&fq=city%3A"'.urlencode($_GET['city']).'"';}
@@ -30,7 +12,10 @@ if (isset($_GET['page'])) {
     $start = ($start-1)*10; 
     $q .= "&start=".$start;
 }
-$url =  get_master_server().'jobs/select?'.$q;
+
+$server = 'zimbor.go.ro';
+$core = 'jobs'; //production
+$url = 'http://' .$server .'/solr/' . $core . '/select/?' . $q;
 $json = file_get_contents($url);
 echo $json;
 ?>
