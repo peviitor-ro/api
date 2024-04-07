55c3de38710247e6d28e2cd892416f314002b3e5
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-04-07 10:20:15 +0300

    fix the server

--- v1/background/index.php
+++ v1/background/index.php
@@ -7,33 +7,7 @@ header("Access-Control-Allow-Origin: *");
      * )
      */
 
-function get_master_server(){
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command ='/server/';
-    $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET',
-            'content' => $data
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
-        }
-}
+
 
 $qs = $_SERVER['QUERY_STRING'];
 $qs = urldecode($qs);
@@ -41,12 +15,8 @@ $qs = urldecode($qs);
 $x = date('w');
 $x = $x-1;
 
-$url =  get_master_server().'tech/select?'.'q.op=OR&q='."cat:".$x.'&rows=100';
-
- 
- 
+$url =  'http://zimbor.go.ro/'.'tech/select?'.'q.op=OR&q='."cat:".$x.'&rows=100';
 $json = file_get_contents($url);
-
 
 echo $json;
 
