a777a54522e10086541f87f15a8186e6ecd4a8a8
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-04-10 19:34:28 +0300

    Create index.php

---
+++ v5/delete/index.php
@@ -0,0 +1,63 @@
+<?php
+
+// Read JSON data from the request body
+$requestBody = file_get_contents('php://input');
+$data = json_decode($requestBody, true);
+
+// Check if 'urls' key exists and is an array
+if (!isset($data['urls']) || !is_array($data['urls'])) {
+    echo 'Invalid or missing "urls" key in the JSON payload.';
+    exit;
+}
+
+// Check if there are URLs to process
+if (empty($data['urls'])) {
+    echo 'No URLs provided in the JSON payload.';
+    exit;
+}
+
+// Solr endpoint URL
+$solrEndpoint = 'http://zimbor.go.ro/solr/jobs/update?_=1617366504771&commitWithin=100&overwrite=true&wt=json';
+
+// Create an array to store the delete operations
+$deleteOperations = [];
+
+// Iterate through URLs and create delete operations
+$url_element = "" ;
+foreach ($data['urls'] as $url) {
+    $url_element.= '"' . $url . '" OR' ;
+}
+
+$url_element = substr($url_element, 0, -3);
+
+// Create delete operation for each URL
+$deleteOperations = [
+    'delete' => [
+        'query' => 'job_link:' . $url_element,
+    ],
+];
+
+
+// Create HTTP context options for the request<
+$options = [
+    'http' => [
+        'header' => "Content-type: application/json",
+        'method' => 'POST',
+        'content' => json_encode($deleteOperations),
+    ],
+];
+
+// Create context stream
+$context = stream_context_create($options);
+
+// Send the request to Solr
+$response = file_get_contents($solrEndpoint, false, $context);
+
+// Check for errors
+if ($response === false) {
+    echo 'Error sending request to Solr';
+} else {
+    // Decode and print the Solr response
+    $solrResponse = json_decode($response, true);
+}; 
+?>
