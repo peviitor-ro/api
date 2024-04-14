f1691f18751257028dd882a753b37de5142c5063
ale23yfm <aleifrim79@gmail.com>
2024-04-14 22:24:21 +0300

    Update index.php

--- v1/companies/index.php
+++ v1/companies/index.php
@@ -1,20 +1,23 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-   /**
-     * @OA\Get(
-     *     path="/v1/companies/", tags={"UI"},
-   *  @OA\Parameter(
-     *                in="query", 
-     *                 name="count",
-     *                 @OA\Schema(
-     *                   type="string"), 
-     *                 example="true"
-     * ),
-     *     @OA\Response(response="200", description="Success")
-     * )
-     */
+/**
+ * @OA\Get(
+ *     path="/v1/companies/", tags={"UI"},
+ *  @OA\Parameter(
+ *                in="query", 
+ *                 name="count",
+ *                 @OA\Schema(
+ *                   type="string"), 
+ *                 example="true"
+ * ),
+ *     @OA\Response(response="200", description="Success")
+ * )
+ */
 
+$server = 'http://zimbor.go.ro';
+$qs
+$url = 
 $url = 'https://api.peviitor.ro/v0/search/?facet.field=company_str&facet.limit=10000&facet=true&fl=company&facet.sort=index&indent=true&q.op=OR&q=*%3A*&rows=0&start=0';
 $string = file_get_contents($url);
 $json = json_decode($string, true);
@@ -22,21 +25,19 @@ $json = json_decode($string, true);
 $companies = $json['facet_counts']['facet_fields']['company_str'];
 
 $results =  new stdClass();
-$results->total = count($companies)/2;
+$results->total = count($companies) / 2;
 $results->companies = array();
 
-for($i=0;$i<count($companies)/2;$i++) {
-    $k=2*$i;
-    $l=2*$i+1;
-    $obj = new stdClass();
-    $obj->name = $companies[$k];   
-    if (isset($_GET['count']))
-     if ($_GET['count']=='true')
-       {
-    $obj->jobs = $companies[$l];
-       }
-    $results->companies[$i] = new stdClass();
-    $results->companies[$i] = $obj;  
- }
+for ($i = 0; $i < count($companies) / 2; $i++) {
+   $k = 2 * $i;
+   $l = 2 * $i + 1;
+   $obj = new stdClass();
+   $obj->name = $companies[$k];
+   if (isset($_GET['count']))
+      if ($_GET['count'] == 'true') {
+         $obj->jobs = $companies[$l];
+      }
+   $results->companies[$i] = new stdClass();
+   $results->companies[$i] = $obj;
+}
 echo json_encode($results);
-?>
