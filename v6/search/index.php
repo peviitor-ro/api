4668cc005f5d9ba93a7b61b11af731e95ba7c3b4
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-05-01 12:17:38 +0300

    Create index.php

---
+++ v6/search/index.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+
+
+
+
+if (isset($_GET['q'])) {$q  = "q=".urlencode($_GET['q']);} else {$q='q=*:*';}
+if (isset($_GET['company'])) {$q .= "&fq=company:".urlencode($_GET['company']);}
+if (isset($_GET['city'])) {$q .= '&fq=city%3A"'.urlencode($_GET['city']).'"';}
+if (isset($_GET['country'])) {$q .= "&fq=country:".urlencode($_GET['country']);}
+if (isset($_GET['county'])) {$q .= "&fq=county:".urlencode($_GET['county']);}
+if (isset($_GET['remote'])) {$q .= "&fq=remote:".urlencode($_GET['remote']);}
+if (isset($_GET['page'])) {
+    $start = $_GET['page'];
+    $start = ($start-1)*10; 
+    $q .= "&start=".$start;
+    $q .= "&rows=12";
+}
+
+$url =  'http://zimbor.go.ro:8985/solr/'.'jobs/select?'.$q;
+$json = file_get_contents($url);
+echo $json;
+?>
