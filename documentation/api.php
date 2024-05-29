17098cfa5d2ad24e7fbde0b35a57ab7c8a429c90
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-05-30 00:16:52 +0300

    swagger - try - 01

--- documentation/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-require($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/vendor/autoload.php');
-$openapi = \OpenApi\scan($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/v1/');
-header('Content-Type: application/json');
-echo $openapi->toJSON();
-
