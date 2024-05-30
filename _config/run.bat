48983456a37ee3575d4ac5e44cf25aa179a99028
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-05-30 23:18:53 +0300

    Update run.bat

--- _config/run.bat
+++ _config/run.bat
@@ -1,3 +1,5 @@
 docker network create --subnet=172.18.0.0/16 mynetwork
 docker run --name apache-container --network mynetwork --ip 172.18.0.11 -d -p 8080:80 -v C:/php:/var/www/html sebiboga/php-apache:1.0.0
 docker run --name solr-container --network mynetwork --ip 172.18.0.10 -d -p 8983:8983 -v c:\solrdata\solr\core:/var/solr sebiboga/peviitor:1.0.0
+docker run --name data-migration --network mynetwork --ip 172.18.0.12 --rm sebiboga/peviitor-data-migration-local:latest
+
