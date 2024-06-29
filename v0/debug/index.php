787e7deb8c7afffdb5b182d09bfb8c6e70981659
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-06-29 21:20:18 +0300

    Create index.php

---
+++ v0/debug/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$url = 'https://zimbor.go.ro:8985/solr/jobs/select?q=*:*&wt=json';
+
+// Încercăm să obținem conținutul de la URL folosind file_get_contents
+$content = file_get_contents($url);
+
+if ($content === false) {
+    // Dacă nu am putut obține conținutul, afișăm un mesaj de eroare
+    echo "Nu am putut obține conținutul de la URL-ul specificat.";
+} else {
+    // Dacă am reușit să obținem conținutul, afișăm conținutul
+    echo $content;
+}
+
+?>
