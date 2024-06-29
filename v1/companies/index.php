7b17fad1623440254505955cecb2b25a6528943e
ale23yfm <aleifrim79@gmail.com>
2024-06-29 20:22:50 +0300

    point to QA

--- v1/companies/index.php
+++ v1/companies/index.php
@@ -23,8 +23,6 @@ $qs = $qs . 'facet.field=company_str';
 $qs = $qs . '&';
 $qs = $qs . 'facet.limit=10000';
 $qs = $qs . '&';
-$qs = $qs . 'facet.sort=index';
-$qs = $qs . '&';
 $qs = $qs . 'facet=true';
 $qs = $qs . '&';
 $qs = $qs . 'fl=company';
@@ -42,6 +40,7 @@ $qs = $qs . '&';
 $qs = $qs . 'useParams=';
 
 $url = 'http://' . $server . '/solr/' . $core . '/select'. $qs;
+
 $string = file_get_contents($url);
 $json = json_decode($string, true);
 
@@ -65,4 +64,5 @@ for($i=0;$i<count($companies)/2;$i++) {
     $results->companies[$i] = $obj;  
  }
 echo json_encode($results);
+
 ?>
