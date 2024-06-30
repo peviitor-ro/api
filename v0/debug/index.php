c9d4e4f64bd025585a7147014beb7f9394c89e49
ale23yfm <157494822+ale23yfm@users.noreply.github.com>
2024-06-30 20:55:31 +0300

    Revert "v0/debug deleted"

---
+++ v0/debug/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$url = 'http://zimbor.go.ro/solr/jobs/select?q=*:*&wt=json';
+
+// Încercăm să obținem conținutul de la URL folosind file_get_contents
+$content = file_get_contents($url);
+
+if ($content === false) {
+    // Dacă nu am putut obține conținutul, afișăm un mesaj de eroare
+    echo "Nu am putut obține conținutul de la URL-ul specificat.";
+} else {
+    // Dacă am reușit să obținem conținutul, afișăm conținutul
+    echo $content;
+}
+
+?>
