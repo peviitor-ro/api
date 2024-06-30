89693916c34a55921b73cab91096acb3144dc149
ale23yfm <aleifrim79@gmail.com>
2024-06-30 21:38:27 +0300

    updated v1/getuser

--- v1/getuser/index.php
+++ v1/getuser/index.php
@@ -1,34 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-function get_master_server(){
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command ='/server/';
-    $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET',
-            'content' => $data
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
-        }
-}
-
 $qs = $_SERVER['QUERY_STRING'];
 $qs = urldecode($qs);
 
