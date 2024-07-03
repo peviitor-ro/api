b20576db1ae04e9c4c14cbf7894dc57bfdfda54d
ale23yfm <aleifrim79@gmail.com>
2024-07-03 22:09:23 +0300

    updated v3/clean

--- v3/clean/index.php
+++ v3/clean/index.php
@@ -1,41 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-    function get_server(){
-        //get the IP of the server
-        //we need a config file to know where is the SOLR
-        require('../../_config/index.php');
-        return $server;
-    }
-
-   function get_master_server(){
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command ='/server/';
-    $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET',
-            'content' => $data
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
-        }
-}
-
  function validate_api_key($key) {
     $method = 'GET';
     $server = get_master_server();
@@ -66,8 +31,6 @@ header("Access-Control-Allow-Origin: *");
      //go to database
      //get the company code based on $token
 
-
-
      $method = 'GET';
      $server = get_master_server();
      $core  = 'auth';
@@ -95,7 +58,7 @@ header("Access-Control-Allow-Origin: *");
  function clean($xcompany) {
    
     $method = 'POST';
-    $server = get_server();
+    $server = 'zimbor.go.ro';
     $core  = 'jobs';
     $command ='/update';
     $qs = '?_=1617366504771&commitWithin=1000&overwrite=true&wt=json';
@@ -103,9 +66,6 @@ header("Access-Control-Allow-Origin: *");
     $data = "{'delete': {'query': 'company:";
         $data.=$xcompany;
     $data.="'}}";
-
-    //echo $data;
-    $url = $server[0].$core.$command.$qs;
 
     $options = array(
         'http' => array(
@@ -116,15 +76,13 @@ header("Access-Control-Allow-Origin: *");
     );
     $context  = stream_context_create($options);
 
-     foreach ($server as $solrurl){
-      $url =  $solrurl.$core.$command.$qs;
-      $result = file_get_contents($url, false, $context);
+    $url =  'http://' . $server . $core . $command . $qs;
+    $result = file_get_contents($url, false, $context);
     if ($result === FALSE) { /* Handle error */ }
-     }
-      
   
     var_dump($result);
  }
+
  // endpoint starts here
     foreach (getallheaders() as $name => $value) {
         if (($name=='apikey'))        {	
