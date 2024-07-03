043cfa91d424f90c6fbec091371b7ca52b9d3a3a
ale23yfm <aleifrim79@gmail.com>
2024-07-03 23:30:50 +0300

    added v0/jobs

---
+++ v0/jobs/index.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+/**
+ * @OA\Get(
+ *     path="/v0/jobs/",
+ *     tags={"machine learning"},
+ *     operationId="getJobs",
+ *     @OA\Parameter(
+ *         in="query",
+ *         name="start",
+ *         @OA\Schema(type="string"),
+ *         example="100"
+ *     ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="Success"
+ *     )
+ * )
+ */
+
+if (isset($_GET["start"])) {
+    $start = $_GET["start"];
+    $qs .= "&start=" . $start;
+}
+
+$server = '172.18.0.10:8983';
+$core = "jobs";
+
+$qs = '?';
+$qs = $qs . 'facet=true';
+$qs = $qs . '&';
+$qs = $qs . 'indent=true';
+$qs = $qs . '&';
+$qs = $qs . 'q.op=OR';
+$qs = $qs . '&';
+$qs = $qs . 'q=*%3A*';
+$qs = $qs . '&';
+$qs = $qs . 'rows=100';
+$qs = $qs . '&';
+$qs = $qs . 'omitHeader=true';
+$qs = $qs . '&';
+$qs = $qs . 'useParams=';
+
+$url = 'http://' . $server . '/solr/' . $core . '/select'. $qs;
+
+$json = file_get_contents($url);
+echo $json;
