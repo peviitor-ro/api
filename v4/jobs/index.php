d1fdc21ef62e44400241b9038f7b6a3c861fc997
ale23yfm <aleifrim79@gmail.com>
2024-07-04 23:25:23 +0300

    update v4/jobs

--- v4/jobs/index.php
+++ v4/jobs/index.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+
     /**
      * @OA\Get(
      *     path="/v3/jobs/", tags={"machine learning"},
@@ -14,39 +15,23 @@ header("Access-Control-Allow-Origin: *");
      *     @OA\Response(response="200", description="Success")
      * )
      */
-   function get_master_server(){
-    $method = 'GET';
-    $server = "https://api.peviitor.ro/";
-    $core  = 'v0';
-    $command ='/server/';
-    $qs = '';
-    $url =  $server.$core.$command.$qs;
-   
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'GET',
-            'content' => $data
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
-    foreach($json as $item)
-        {
-            if ($item->status=="up"){
-                return $item->server;
-                break;
-            }
-        }
-}
 
+$method = 'GET';
+$server = 'zimbor.go.ro';
+$core  = 'jobs';
 
-$qs = "q=*%3A*&rows=100&omitHeader=true";
-//$qs = urldecode($qs);
+$qs = 'q=*%3A*';
+$qs = $qs . '&';
+$qs = $qs . 'rows=100';
+$qs = $qs . '&';
+$qs = $qs . 'omitHeader=true';
+
+$url =  'http://' . $server . '/solr/' . '/select?' . $qs;
+
 if (isset($_GET["start"])) {$start=$_GET["start"];$qs.="&start=".$start;}
-$url =  get_master_server().'jobs/select?'.$qs;
+
 $json = file_get_contents($url);
+
 echo $json;
+
 ?>
