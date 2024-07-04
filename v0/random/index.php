1acd6240b892f79f1296cb4ee08153bfacf3e4c4
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-04 19:11:10 +0300

    adding error handling for v0 random

--- v0/random/index.php
+++ v0/random/index.php
@@ -1,38 +1,36 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: *');
 
-// Server settings
 $server = '172.18.0.10:8983'; //DEV
 $core = 'jobs';
 
 try {
-    // Get the max count of jobs
     $url = 'http://' .$server .'/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=0';
     $string = @file_get_contents($url);
     if ($string === FALSE) {
-        throw new Exception('SOLR server is down', 503);
+        // Force HTTP status code to be 503
+        header("HTTP/1.1 503 Service Unavailable");
+        throw new Exception('SOLR server in DEV is down', 503);
     }
+
     $json = json_decode($string, true);
-
     $max = $json['response']['numFound'];
-    $start = rand(0, $max-1); // Select a random job
+    $start = rand(0, $max-1); 
 
-    // Fetch the selected job in solr
     $url = 'http://' .$server .'/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=1' . '&start=' . $start . '&omitHeader=true';
     $json = @file_get_contents($url);
     if ($json === FALSE) {
         list($version,$status,$msg) = explode(' ', $http_response_header[0], 3);
+        // Force HTTP status code to be 503
+        header("HTTP/1.1 503 Service Unavailable");
         throw new Exception('Your call to Solr failed and returned HTTP status: ' . $status, $status);
     }
 
-    echo $json; // Display job
-
+    echo $json;
 } catch (Exception $e) {
-    http_response_code($e->getCode());
-    echo $e->getMessage();
+    echo json_encode(['error' => $e->getMessage(), 'code' => $e->getCode()]);
     exit;
 }
 ?>
