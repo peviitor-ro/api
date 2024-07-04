2d19daa862de4d231566b96129db36ddeb31896a
ale23yfm <aleifrim79@gmail.com>
2024-07-04 23:18:32 +0300

    updated v3/clean

--- v3/clean/index.php
+++ v3/clean/index.php
@@ -1,13 +1,20 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
- function validate_api_key($key) {
+function validate_api_key($key) {
     $method = 'GET';
-    $server = get_master_server();
+    $server = 'zimbor.go.ro';
     $core  = 'auth';
     $command ='/select';
-    $qs = '?q.op=OR&q=apikey%3A"'.$key.'"&rows=0';
-    $url =  $server.$core.$command.$qs;
+
+    $qs = '?';
+    $qs = $qs . 'q.op=OR';
+    $qs = $qs . '&';
+    $qs = $qs . 'q=apikey%3A"';
+    $qs = $qs . $key;
+    $qs = $qs . '"&rows=0';
+
+    $url = 'http://' . $server . $core . $command . $qs;
    
     $options = array(
         'http' => array(
@@ -16,55 +23,77 @@ header("Access-Control-Allow-Origin: *");
             'content' => $data
         )
     );
+
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-    $json = json_decode($result);
 
-     $y = $json->response->numFound; 
+    if ($result === FALSE) { /* Handle error */ }
+
+    $json = json_decode($result);
+    $y = $json->response->numFound; 
+
     if ($y==1) {$x = true;}
     if ($y==0) {$x = false;}
-  return $x;
- }
 
- function get_company($token) {
-     //go to database
-     //get the company code based on $token
-
-     $method = 'GET';
-     $server = get_master_server();
-     $core  = 'auth';
-     $command ='/select';
-     $qs = '?q.op=OR&q=apikey%3A"'.$token.'"%26rows%3D1';
-     $url =  $server.$core.$command.$qs;
-    
-     $options = array(
-         'http' => array(
-             'header'  => "Content-type: application/json\r\n",
-             'method'  => 'GET',
-             'content' => $data
-         )
-     );
-     $context  = stream_context_create($options);
-     $result = file_get_contents($url, false, $context);
-     if ($result === FALSE) { /* Handle error */ }
-      $json = json_decode($result);
-    
-    $x=$json->response->docs[0]->company[0];
     return $x;
- }
+}
 
+function get_company($token) {
+    //go to database
+    //get the company code based on $token
 
- function clean($xcompany) {
-   
+    $method = 'GET';
+    $server = 'zimbor.go.ro';
+    $core  = 'auth';
+    $command ='/select';
+
+    $qs = '?';
+    $qs = $qs . 'q.op=OR';
+    $qs = $qs . '&';
+    $qs = $qs . 'q=apikey%3A"';
+    $qs = $qs . $token;
+    $qs = $qs . '"%26rows%3D1';
+
+    $url = 'http://' . $server . $core . $command . $qs;
+    
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'GET',
+            'content' => $data
+        )
+    );
+
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === FALSE) { /* Handle error */ }
+
+    $json = json_decode($result);
+    $x=$json->response->docs[0]->company[0];
+
+    return $x;
+}
+
+function clean($xcompany) {
     $method = 'POST';
     $server = 'zimbor.go.ro';
     $core  = 'jobs';
     $command ='/update';
-    $qs = '?_=1617366504771&commitWithin=1000&overwrite=true&wt=json';
+
+    $qs = '?';
+    $qs = $qs . '_=1617366504771';
+    $qs = $qs . '&';
+    $qs = $qs . 'commitWithin=1000';
+    $qs = $qs . '&';
+    $qs = $qs . 'overwrite=true';
+    $qs = $qs . '&';
+    $qs = $qs . 'wt=json';
+
+    $url =  'http://' . $server . $core . $command . $qs;
     
     $data = "{'delete': {'query': 'company:";
-        $data.=$xcompany;
+    $data.=$xcompany;
     $data.="'}}";
 
     $options = array(
@@ -74,16 +103,16 @@ header("Access-Control-Allow-Origin: *");
             'content' => $data
         )
     );
-    $context  = stream_context_create($options);
+    $context = stream_context_create($options);
 
-    $url =  'http://' . $server . $core . $command . $qs;
     $result = file_get_contents($url, false, $context);
+    
     if ($result === FALSE) { /* Handle error */ }
   
     var_dump($result);
- }
+}
 
- // endpoint starts here
+// endpoint starts here
     foreach (getallheaders() as $name => $value) {
         if (($name=='apikey'))        {	
           if (validate_api_key($value)==true)
@@ -93,4 +122,5 @@ header("Access-Control-Allow-Origin: *");
               } else {echo "apikey error";}
                                       }
     } 
+    
 ?>
