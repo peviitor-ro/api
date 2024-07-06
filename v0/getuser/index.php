4b88fb23409f2f3ad051996da7921a9baf0df779
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-06 20:19:48 +0300

    enhance CORS

--- v0/getuser/index.php
+++ v0/getuser/index.php
@@ -1,5 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: *');
 
 if (isset($_GET['user']))
 {
