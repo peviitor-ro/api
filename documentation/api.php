fd3ecd6e6051d9ecdced397bc2bb8fc25b21fe35
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-07 11:22:29 +0300

    add cord to api.php for swagger definition

--- documentation/api.php
+++ documentation/api.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/vendor/autoload.php');
 $openapi = \OpenApi\scan($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/v1/');
 header('Content-Type: application/json');
