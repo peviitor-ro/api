0f7103115e1b2c28cd5b2f365669cded6d313fcc
ale23yfm <aleifrim79@gmail.com>
2024-07-09 18:16:33 +0300

    added v3/logo/delete

---
+++ v3/logo/delete/index.php
@@ -0,0 +1,35 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$method = 'POST';
+$server = 'zimbor.go.ro';
+$core  = 'auth';
+$command ='/update';
+
+$qs = '?';
+$qs = $qs . '_=1617366504771';
+$qs = $qs . '&';
+$qs = $qs . 'commitWithin=1000';
+$qs = $qs . '&';
+$qs = $qs . 'overwrite=true';
+$qs = $qs . '&';
+$qs = $qs . 'wt=json';
+
+$url = 'http://' . $server . $core . $command . $qs;
+ 
+$company = $_POST['company'];
+$data = "{'delete': {'query': 'id:".$company."'}}";
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => $data
+    )
+);
+$context  = stream_context_create($options);
+
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { echo $result; }
+
+?>
