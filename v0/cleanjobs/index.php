f7fa5f4318e233cfc96f594787a2a9699cd7d585
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-14 14:05:19 +0300

    Update index.php

--- v0/cleanjobs/index.php
+++ v0/cleanjobs/index.php
@@ -1,28 +1,17 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-// Fetch company from DELETE request
-parse_str(file_get_contents("php://input"), $_DELETE);
-$company = $_DELETE['company'] ?? '';
-
 require_once '../config.php';
 
-$method = 'DELETE';
+$method = 'POST';
 $core  = 'jobs';
 $command = '/update';
 
-$qs = '?';
-$qs = $qs . '_=1617366504771';
-$qs = $qs . '&';
-$qs = $qs . 'commitWithin=1000';
-$qs = $qs . '&';
-$qs = $qs . 'overwrite=true';
-$qs = $qs . '&';
-$qs = $qs . 'wt=json';
+$qs = '?_=' . time() . '&commitWithin=1000&overwrite=true&wt=json';
 
 $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
 
-$data = json_encode(['delete' => ['query' => 'hiringOrganization.name:"' . $company . '"']]);
+$data = json_encode(['delete' => ['query' => '*:*']]); //New Format
 
 $options = array(
     'http' => array(
@@ -35,15 +24,12 @@ $options = array(
 $context  = stream_context_create($options);
 
 try {
-    if (empty($company)) {
-        header("HTTP/1.1 400 Bad Request");
-        echo json_encode(['error' => 'Company name is required', 'code' => 400]);
-        exit;
-    }
+
     $json = @file_get_contents($url, false, $context);
 
     if ($json === FALSE) {
         list($version, $status, $msg) = explode(' ', $http_response_header[0], 3);
+        // Force HTTP status code to be 503
         header("HTTP/1.1 503 Service Unavailable");
         throw new Exception('Your call to Solr failed and returned HTTP status: ' . $status, $status);
     }
@@ -51,5 +37,5 @@ try {
     echo $json;
 } catch (Exception $e) {
     echo json_encode(['error' => $e->getMessage(), 'code' => $e->getCode()]);
-    exit;
 }
+?>
