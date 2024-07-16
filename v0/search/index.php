9ab07c5a1b70056d0cbd308624a52c05c2330179
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-16 22:01:03 +0300

    fix when q is not sent to v0 search

--- v0/search/index.php
+++ v0/search/index.php
@@ -7,7 +7,9 @@ $core = 'jobs';
 $qs = '?';
 $qs = $qs . $_SERVER['QUERY_STRING'];
 
+if ( $_SERVER['QUERY_STRING'] == "page=1") {$qs .= "&q=*%3A*";} 
 $url = 'http://' . $server . '/solr/' . $core . '/select' . $qs;
+
 
 $json = file_get_contents($url);
 echo $json;
