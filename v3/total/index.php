c85e1e33470a77d07b2b10e5a5cf43117a192be0
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2024-07-19 17:35:38 +0300

    Update index.php

--- v3/total/index.php
+++ v3/total/index.php
@@ -1,13 +1,37 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$url = 'https://api.peviitor.ro/v0/search/?facet.field=company_str&facet.limit=10000&facet=true&fl=company&indent=true&q.op=OR&q=*%3A*&rows=0&start=0';
+require_once '../config.php';
+
+$core = "jobs";
+     
+$qs = '?';
+$qs = $qs . 'facet.field=company_str';
+$qs = $qs . '&';
+$qs = $qs . 'facet.limit=2000000';
+$qs = $qs . '&';
+$qs = $qs . 'facet=true';
+$qs = $qs . '&';
+$qs = $qs . 'fl=company';
+$qs = $qs . '&';
+$qs = $qs . 'indent=true';
+$qs = $qs . '&';
+$qs = $qs . 'q.op=OR';
+$qs = $qs . '&';
+$qs = $qs . 'q=*%3A*';
+$qs = $qs . '&';
+$qs = $qs . 'rows=0';
+$qs = $qs . '&';
+$qs = $qs . 'start=0';
+$qs = $qs . '&';
+$qs = $qs . 'useParams=';
+     
+$url = 'http://' . $server . '/solr/' . $core . '/select' . $qs;
+
 $string = file_get_contents($url);
 $json = json_decode($string, true);
 
 $companies = $json['facet_counts']['facet_fields']['company_str'];
-
-
 
 $obj = new stdClass();
 $obj->total = new stdClass();
@@ -15,6 +39,5 @@ $obj->total -> jobs = ''.$json['response']['numFound'];
 $obj->total -> companies = ''.count($companies)/2;
 
 echo json_encode($obj);
-
 
 ?>
