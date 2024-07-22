71c6710c5fa59b37e7d906f14fdd939c5ab8a6ef
ale23yfm <aleifrim79@gmail.com>
2024-07-22 15:02:02 +0300

    added random message in swagger-ui

--- v0/random/index.php
+++ v0/random/index.php
@@ -8,7 +8,8 @@ require_once '../config.php';
 $core = 'jobs';
 
 try {
-    $url = 'http://' .$server .'/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=0';
+    // Request the total number of documents
+    $url = 'http://' . $server . '/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=0';
     $string = @file_get_contents($url);
     if ($string === FALSE) {
         // Force HTTP status code to be 503
@@ -18,12 +19,20 @@ try {
 
     $json = json_decode($string, true);
     $max = $json['response']['numFound'];
-    $start = rand(0, $max-1); 
+    
+    // If no documents are found, return a specific message
+    if ($max == 0) {
+        echo json_encode(['message' => 'There are no jobs to display']);
+        exit;
+    }
 
-    $url = 'http://' .$server .'/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=1' . '&start=' . $start . '&omitHeader=true';
+    // Randomly select a document if there are jobs
+    $start = rand(0, $max - 1); 
+
+    $url = 'http://' . $server . '/solr/' . $core . '/select?q=' . urlencode('*:*') . '&rows=1' . '&start=' . $start . '&omitHeader=true';
     $json = @file_get_contents($url);
     if ($json === FALSE) {
-        list($version,$status,$msg) = explode(' ', $http_response_header[0], 3);
+        list($version, $status, $msg) = explode(' ', $http_response_header[0], 3);
         // Force HTTP status code to be 503
         header("HTTP/1.1 503 Service Unavailable");
         throw new Exception('Your call to Solr failed and returned HTTP status: ' . $status, $status);
