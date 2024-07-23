21d50a00e32d51c585b40d9c9d28ac7a805c4d4b
ale23yfm <aleifrim79@gmail.com>
2024-07-23 22:59:13 +0300

    added back-up servers

--- v3/config.php
+++ v3/config.php
@@ -1,6 +1,15 @@
 <?php
 
+//main server
 $server = 'zimbor.go.ro';
+
+//QA server
+//$server = 'zimbor.go.ro:8985';
+
+//Ale's server
 //$server = '84.117.157.10:8983';
+
+//Alex's server
+//$server = 'srvalx.duckdns.org:8983';
 
 ?>
