e5e89fc9761a87790d94b441b65fb21da695a5b4
ale23yfm <aleifrim79@gmail.com>
2024-08-20 19:30:16 +0300

    added     require_once '../config.php';

--- v4/companies/index.php
+++ v4/companies/index.php
@@ -1,29 +1,41 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-   /**
-     * @OA\Get(
-     *     path="/v1/companies/", tags={"UI"},
-   *  @OA\Parameter(
-     *                in="query", 
-     *                 name="count",
-     *                 @OA\Schema(
-     *                   type="string"), 
-     *                 example="true"
-     * ),
-     *     @OA\Response(response="200", description="Success")
-     * )
-     */
+/**
+ * @OA\Get(
+ *     path="/v1/companies/", tags={"UI"},
+ *  @OA\Parameter(
+ *                in="query", 
+ *                 name="count",
+ *                 @OA\Schema(
+ *                   type="string"), 
+ *                 example="true"
+ * ),
+ *     @OA\Response(response="200", description="Success")
+ * )
+ */
+require_once '../config.php';
 
-$url = 'https://api.peviitor.ro/v0/search/?facet.field=company_str&facet=on&q=*%3A*&rows=0';
+$core = 'jobs';
+
+$qs = '?';
+$qs .= 'facet.field=company_str';
+$qs .= '&';
+$qs .= 'facet=on';
+$qs .= '&';
+$qs .= 'q=*%3A*';
+$qs .= '&';
+$qs .= 'rows=0';
+
+$url = 'http://' . $server . '/solr/' . $core . '/select' . $qs;
+
 $string = file_get_contents($url);
 $json = json_decode($string, true);
 $companies = $json['facet_counts']['facet_fields']['company_str'];
 $results = array();
- 
-for($i=0;$i<count($companies)/2;$i++) {
-    $k=2*$i;
-    $results[$i] = $companies[$k];
-  }
+
+for ($i = 0; $i < count($companies) / 2; $i++) {
+  $k = 2 * $i;
+  $results[$i] = $companies[$k];
+}
 echo json_encode($results);
-?>
--- v4/update/index.php
+++ v4/update/index.php
@@ -2,21 +2,22 @@
 header("Access-Control-Allow-Origin: *");
 
 
-function validate_api_key($key) {
+function validate_api_key($key)
+{
     $method = 'GET';
 
     require_once '../config.php';
 
     $core  = 'auth';
-    $command ='/select';
+    $command = '/select';
 
     $qs = '?';
-    $qs = $qs . 'q.op=OR';
-    $qs = $qs . '&';
-    $qs = $qs . 'q=apikey%3A"';
-    $qs = $qs . $key;
-    $qs = $qs . '"&rows=0';
-    
+    $qs .= 'q.op=OR';
+    $qs .= '&';
+    $qs .= 'q=apikey%3A"';
+    $qs .= $key;
+    $qs .= '"&rows=0';
+
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/json\r\n",
@@ -26,39 +27,46 @@ function validate_api_key($key) {
     );
 
     $context  = stream_context_create($options);
-    
+
     $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
-    
+
     $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { /* Handle error */ }
-     
+    if ($result === FALSE) { /* Handle error */
+    }
+
     $json = json_decode($result);
-    $y = $json->response->numFound; 
-    
-    if ($y==1) {$x = true;}
-    if ($y==0) {$x = false;}
-    
+    $y = $json->response->numFound;
+
+    if ($y == 1) {
+        $x = true;
+    }
+    if ($y == 0) {
+        $x = false;
+    }
+
     return $x;
 }
 
 
-function get_user_from_api_key($key) {
+function get_user_from_api_key($key)
+{
     $method = 'GET';
 
     require_once '../config.php';
 
     $core  = 'auth';
-    $command ='/select';
+    $command = '/select';
 
     $qs = '?';
-    $qs = $qs . 'q.op=OR';
-    $qs = $qs . '&';
-    $qs = $qs . 'q=apikey%3A"';
-    $qs = $qs . $key;
-    $qs = $qs . '"&rows=1';
+    $qs .= 'q.op=OR';
+    $qs .= '&';
+    $qs .= 'q=apikey%3A"';
+    $qs .= $key;
+    $qs .= '"&';
+    $qs .= 'rows=1';
 
     $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
-   
+
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/json\r\n",
@@ -69,29 +77,36 @@ function get_user_from_api_key($key) {
 
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-    
-    if ($result === FALSE) { /* Handle error */ }
+
+    if ($result === FALSE) { /* Handle error */
+    }
 
     $json = json_decode($result);
-    $y = $json->response->numFound; 
-    
-    if ($y==1) {$x = $json->response->docs[0]->id;}
-    if ($y==0) {$x = false;}
-    
+    $y = $json->response->numFound;
+
+    if ($y == 1) {
+        $x = $json->response->docs[0]->id;
+    }
+    if ($y == 0) {
+        $x = false;
+    }
+
     return $x;
 }
 
-function get_company($token) {
+function get_company($token)
+{
     $x = $company = $_POST['company'];
     return $x;
 }
 
 
-function discord_webhook($msg) {
-    $msg .= ' UPDATE in PRODUCTION '.date("l d-m-Y H:i:s");
+function discord_webhook($msg)
+{
+    $msg .= ' UPDATE in PRODUCTION ' . date("l d-m-Y H:i:s");
     $method = 'POST';
     $url = "https://discord.com/api/webhooks/1127143279977308240/etcQT4Roo02_6sy38WwUWwUmaNGKEylEJxJuq_bWw0HZLiynXKPLAt3qnyWpGnRd6X8Y";
-    $data = '{"content": "'.$msg.'"}';
+    $data = '{"content": "' . $msg . '"}';
 
     $options = array(
         'http' => array(
@@ -103,45 +118,46 @@ function discord_webhook($msg) {
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
 
-    if ($result === FALSE) { /* Handle error */ }
- 
+    if ($result === FALSE) { /* Handle error */
+    }
 }
 
- 
-function update($key) {
+
+function update($key)
+{
     $method = 'POST';
 
     require_once '../config.php';
 
     $core  = 'jobs';
-    $command ='/update';
+    $command = '/update';
 
     $qs = '?';
-    $qs = $qs . '_=1617366504771';
-    $qs = $qs . '&';
-    $qs = $qs . 'commitWithin=1000';
-    $qs = $qs . '&';
-    $qs = $qs . 'overwrite=true';
-    $qs = $qs . '&';
-    $qs = $qs . 'wt=json';
-     
+    $qs .= '_=1617366504771';
+    $qs .= '&';
+    $qs .= 'commitWithin=1000';
+    $qs .= '&';
+    $qs .= 'overwrite=true';
+    $qs .= '&';
+    $qs .= 'wt=json';
+
     $data = file_get_contents('php://input');
-    
+
     $json = json_decode($data);
-  
+
     foreach ($json as $item) {
-    $item->job_title  = html_entity_decode($item->job_title);
-    $item->country    = str_replace("Romania","România",$item->country);
-	$item->city    = str_replace("Cluj Napoca","Cluj-Napoca",$item->city);
-	$item->city    = str_replace("Bucharest","București",$item->city);
-	$item->city    = str_replace("Bucuresti","București",$item->city);
-    $item->id = md5($item->job_link)."";
-    $company = $item->company; 
+        $item->job_title  = html_entity_decode($item->job_title);
+        $item->country    = str_replace("Romania", "România", $item->country);
+        $item->city    = str_replace("Cluj Napoca", "Cluj-Napoca", $item->city);
+        $item->city    = str_replace("Bucharest", "București", $item->city);
+        $item->city    = str_replace("Bucuresti", "București", $item->city);
+        $item->id = md5($item->job_link) . "";
+        $company = $item->company;
     }
-    
+
 
     $data = json_encode($json);
-    
+
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/json\r\n",
@@ -150,29 +166,30 @@ function update($key) {
         )
     );
 
-    $msg = $company.' user: '.get_user_from_api_key($key);
+    $msg = $company . ' user: ' . get_user_from_api_key($key);
     discord_webhook($msg);
     $context  = stream_context_create($options);
-     
+
     $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
 
     $result = file_get_contents($url, false, $context);
 
-    if ($result === FALSE) { /* Handle error */ }
-         
+    if ($result === FALSE) { /* Handle error */
+    }
+
     var_dump($result);
 }
- 
+
 
 // endpoint starts here
 
 foreach (getallheaders() as $name => $value) {
-    if (($name=='apikey'))        {	
-        if (validate_api_key($value)==true)
-            {     
-                    
-                update($value);
-            } else {echo "apikey error";}
+    if (($name == 'apikey')) {
+        if (validate_api_key($value) == true) {
+
+            update($value);
+        } else {
+            echo "apikey error";
+        }
     }
-} 
-?>
+}
