2303885ef857b6aaad395d3c7095afa4e05a2553
ale23yfm <aleifrim79@gmail.com>
2024-12-20 18:29:23 +0200

    solved deletion by id

--- v0/logo/clean/index.php
+++ v0/logo/clean/index.php
@@ -1,31 +1,39 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
 require_once '../../config.php';
 
-$core = 'auth';
+$core = 'auth'; // Core name
 $command = '/update';
 
 $qs = '?_=' . time(); // Use current time as a cache buster
 $qs .= '&commitWithin=1000';
-$qs .= '&overwrite=true';
 $qs .= '&wt=json';
 
 $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
 
-$company = $_POST['company'];
+// Get the required parameters
+$id = $_GET['id'] ?? ''; // Document ID to update
+$field = 'url';          // Field to remove (hardcoded for this endpoint)
 
-if (empty($company)) {
+if (empty($id)) {
     header("HTTP/1.1 400 Bad Request");
-    echo json_encode(['error' => 'Company name is required', 'code' => 400]);
+    echo json_encode(['error' => 'Document ID is required', 'code' => 400]);
     exit;
 }
 
-$data = json_encode(['delete' => ['query' => 'id:' . $company]]);
+// Create the atomic update payload
+$data = json_encode([
+    [
+        "id" => $id,
+        $field => ["set" => null] // Use Solr's atomic update syntax to remove the field
+    ]
+]);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -53,5 +61,4 @@ if ($http_code >= 400) {
 }
 
 echo $response;
-
 ?>
