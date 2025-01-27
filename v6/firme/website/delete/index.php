55105191f73f409e0b6aa9f3cfde030e0e89597d
Boga Sebastian Nicolae <sebastian.boga@outlook.com>
2025-01-27 09:17:13 +0200

    Update index.php

--- v6/firme/website/delete/index.php
+++ v6/firme/website/delete/index.php
@@ -1,37 +1,55 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+// Lista originilor permise
+$allowed_origins = ['https://peviitor-ro.github.io'];
+
+// Determinarea originii cererii
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+} else {
+    http_response_code(403); // Forbidden
+    exit('Origin not allowed');
+}
+
+// Setează header-urile corespunzătoare pentru CORS
 header("Access-Control-Allow-Methods: DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Define Solr server settings
+// Tratamentul cererilor preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Oprește execuția scriptului după trimiterea headerelor
+    http_response_code(200);
+    exit;
+}
+
+// Definirea configurației serverului Solr
 $server = 'zimbor.go.ro';
 $core = 'firme';
 $command = '/update';
 
-$qs = '?commit=true&wt=json'; // Commit changes immediately and request JSON response
+$url = "http://$server/solr/$core$command?commit=true&wt=json";
 
-$url = "http://$server/solr/$core$command$qs";
-
-// Get DELETE data; Usually, data isn't sent in the body of a DELETE request, so use query parameters or URL segments
+// Primirea datelor din cererea DELETE; de obicei nu se trimite corp în cererile DELETE, așa că folosim parametrii din URL
 $input = file_get_contents("php://input");
 $data = json_decode($input, true);
 $id = isset($data['id']) ? htmlspecialchars($data['id']) : null;
 
-// Check if the `id` is provided
+// Verifică dacă este furnizat ID-ul
 if (empty($id)) {
     http_response_code(400); // Bad Request
     echo json_encode(["error" => "Missing required field 'id'."]);
     exit;
 }
 
-// Preparing data for removing the 'website' field from the document
+// Prepară datele pentru a elimina câmpul 'website' din document
 $item = new stdClass();
 $item->id = $id;
-$item->website = ['set' => null]; // Set 'website' to null to remove it
+$item->website = ['set' => null]; // Setarea câmpului 'website' la null pentru a-l elimina
 
+// Codifică datele în JSON pentru trimitere
 $json_data = json_encode([$item]);
 
-// Setting HTTP context options for the POST request to Solr
+// Configurația contextului pentru cererea POST către Solr
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/json\r\n",
@@ -43,6 +61,7 @@ $options = array(
 $context = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
+// Verifică rezultatul și tratează erorile
 if ($result === FALSE) {
     http_response_code(500); // Internal Server Error
     echo json_encode(["error" => "Failed to connect to Solr"]);
