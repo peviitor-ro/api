1faf8e5cd68b81a1a28564c283554812f3e19b8e
ale23yfm <aleifrim79@gmail.com>
2025-02-02 12:14:44 +0200

    fix random

--- v0/random/index.php
+++ v0/random/index.php
@@ -4,6 +4,19 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: *');
 header('Content-Type: application/json; charset=utf-8');
 
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+// Ensure the request is GET
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(["error" => "Only GET method is allowed"]);
+    exit;
+}
+
 require_once '../config.php';
 
 $core = 'jobs';
