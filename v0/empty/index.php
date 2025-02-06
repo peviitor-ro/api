d0682df680e0d7883bf6cd99db1a89ad36e8b1c5
ale23yfm <aleifrim79@gmail.com>
2025-02-06 19:05:50 +0200

    changed 503 error

--- v0/empty/index.php
+++ v0/empty/index.php
@@ -12,16 +12,6 @@ $qs = '?_=' . time() . '&commitWithin=1000&overwrite=true&wt=json';
 
 $url = 'http://' . $server . '/solr/' . $core . $command . $qs;
 
-$string = @file_get_contents($url);
-if ($string === FALSE) {
-    http_response_code(503);
-    echo json_encode([
-        "error" => "SOLR server in DEV is down",
-        "code" => 503
-    ]);
-    exit;
-}
-
 $data = json_encode(['delete' => ['query' => '*:*']]); //New Format
 
 $options = array(
@@ -35,18 +25,27 @@ $options = array(
 $context  = stream_context_create($options);
 
 try {
-    //Count the jobs before deletion
-    $countUrl = 'http://' . $server . '/solr/' . $core . '/select?q=*:*&wt=json&rows=0';  // Count all jobs
+    // Count the jobs before deletion
+    $countUrl = 'http://' . $server . '/solr/' . $core . '/select?q=*:*&wt=json&rows=0';  
     $countJson = @file_get_contents($countUrl);
-    $countResponse = json_decode($countJson, true);
-    $jobCount = $countResponse['response']['numFound'];
+    if ($countJson === false) {
+        throw new Exception("Solr is down or unreachable", 503);
+        exit;
+    }
 
-    //Count companies
+    $countResponse = json_decode($countJson, true);
+    $jobCount = $countResponse['response']['numFound'] ?? 0;
+
+    // Count companies
     $qsCompanies = '?facet.field=company_str&facet.limit=2000000&facet=true&fl=company&q=*%3A*&rows=0';
     $companyUrl = 'http://' . $server . '/solr/' . $core . '/select' . $qsCompanies;
     $companyJson = @file_get_contents($companyUrl);
+    if ($companyJson === false) {
+        throw new Exception("Solr is down or unreachable", 503);
+    }
+
     $companyResponse = json_decode($companyJson, true);
-    $companies = $companyResponse['facet_counts']['facet_fields']['company_str'];
+    $companies = $companyResponse['facet_counts']['facet_fields']['company_str'] ?? [];
 
     $companyCount = 0;
     for ($i = 1; $i < count($companies); $i += 2) {
@@ -54,14 +53,19 @@ try {
             $companyCount++;
         }
     }
-    $json = @file_get_contents($url, false, $context);
 
-    echo $json;
+    // Delete jobs
+    $json = @file_get_contents($url, false, $context);
+    if ($json === false) {
+        throw new Exception("Failed to send delete request to Solr", 503);
+    }
+
     echo json_encode([
         'message' => 'Jobs deleted successfully',
         'jobsDeleted' => $jobCount,
         'companiesDeleted' => $companyCount
     ]);
 } catch (Exception $e) {
+    http_response_code($e->getCode() ?: 500);
     echo json_encode(['error' => $e->getMessage(), 'code' => $e->getCode()]);
 }
