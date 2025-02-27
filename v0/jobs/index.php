cd0cce03a2375c1cef94184ff81cb1e9f26d52b2
ale23yfm <aleifrim79@gmail.com>
2025-02-27 23:58:34 +0200

    changed 200 to 400

--- v0/jobs/index.php
+++ v0/jobs/index.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 }
 
 require_once '../config.php';
-    
+
 $core = "jobs";
 
 // Parametrii pentru interogarea Solr
@@ -33,7 +33,11 @@ if (isset($_GET["rows"])) {
     $rows = $_GET["rows"];
     if (!is_numeric($rows) || $rows <= 0) {
         // Dacă rows nu este valid, returnează o eroare
-        echo json_encode(["error" => "You must provide a positive number for 'rows'"]);
+        http_response_code(400);
+        echo json_encode([
+            "error" => "You must provide a positive number for 'rows'",
+            "code" => 400
+        ]);
         exit;
     }
 }
@@ -73,4 +77,3 @@ unset($json['facet_counts']);
 
 // Returnează doar partea relevantă din răspuns
 echo json_encode($json);
-?>
