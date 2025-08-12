46a6a9c54561e53c7417997d510dbdeeb8667549
ale23yfm <aleifrim79@gmail.com>
2025-08-12 23:16:55 +0300

    changed the header

--- v0/suggest/index.php
+++ v0/suggest/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: *');
 header('Content-Type: application/json; charset=utf-8');
