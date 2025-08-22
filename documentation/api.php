50c17b3ad983c31987c206be428c6ddc8398e1c4
Ifrim Alexandra <aleifrim79@gmail.com>
2025-08-22 10:23:27 +0300

    deleted unnecesary files

--- documentation/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-require($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/vendor/autoload.php');
-$openapi = \OpenApi\scan($_SERVER['DOCUMENT_ROOT'].'/apipeviitorro/v1/');
-header('Content-Type: application/json');
-echo $openapi->toJSON();
-
