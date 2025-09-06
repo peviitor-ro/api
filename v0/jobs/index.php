2d6b8a3b152186f5a9408f5f47abbc450e0ff21b
Ifrim Alexandra <aleifrim79@gmail.com>
2025-09-06 18:20:05 +0300

    deleted cors file and added headers

--- v0/jobs/index.php
+++ v0/jobs/index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=utf-8');
+
 require_once __DIR__ . '/../bootstrap.php';
 $GLOBALS['solr'] = getSolrCredentials('LOCAL');
 
--- v0/random/index.php
+++ v0/random/index.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: *');
+header('Content-Type: application/json; charset=utf-8');
+
 require_once __DIR__ . '/../bootstrap.php';
 $GLOBALS['solr'] = getSolrCredentials('LOCAL');
 
