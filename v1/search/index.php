447331663613b3b33f18f041307601e3cf898b7c
Sebastian Boga <sebitestb@gmail.com>
2026-03-12 20:59:25 +0200

    Fix: Use HTTPS for Solr connection

--- v1/search/index.php
+++ v1/search/index.php
@@ -129,7 +129,7 @@ try {
     }
 
     $core = 'job';
-    $base = "http://$PROD_SERVER/solr/$core/select";
+    $base = "https://$PROD_SERVER/solr/$core/select";
     $url  = $base . '?' . buildSolrQuery($params, $start, $rows);
 
     error_log("JOB CORE URL: $url");
